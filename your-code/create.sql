5ba43282da593bada0a8d9d2e32c0c005d2a94a6
eduarde1 <e.castrobermudez@comunidad.unam.mx>
2019-07-09 01:18:12 -0500

    .

--- your-code/create.sql
+++ your-code/create.sql
@@ -3,7 +3,7 @@ USE sales;
 
 CREATE TABLE cars (CarID int(5) primary key NOT NULL, Manufacturer VARCHAR(50) NOT NULL, Model VARCHAR(50) NOT NULL, YearOfManufacture CHAR(5) NOT NULL, Plates VARCHAR(10), Color VARCHAR(10));
 
-CREATE customers (CustomerID int(5) primary key NOT NULL, CustomerName VARCHAR(50) NOT NULL, PhoneNumber VARCHAR(20) NOT NULL, Email VARCHAR(50), Address VARCHAR(50) NOT NULL, City VARCHAR(50) NOT NULL, State VARCHAR(50) NOT NULL, Country VARCHAR(50) NOT NULL, ZipCode VARCHAR(10) NOT NULL);
+CREATE TABLE customers (CustomerID int(5) primary key NOT NULL, CustomerName VARCHAR(50) NOT NULL, PhoneNumber VARCHAR(20) NOT NULL, Email VARCHAR(50), Address VARCHAR(50) NOT NULL, City VARCHAR(50) NOT NULL, State VARCHAR(50) NOT NULL, Country VARCHAR(50) NOT NULL, ZipCode VARCHAR(10) NOT NULL);
 
 CREATE TABLE salespeople (SalesPersonID int(5) primary key NOT NULL, VendorName VARCHAR(50) NOT NULL), StoreLocation VARCHAR(50));
 
